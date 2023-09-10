03f9e99f4035f346c62cb19b3e52076b21e50f4e
chris-2525 <144377615+chris-2525@users.noreply.github.com>
2023-09-09 21:44:32 -0400

    Update clinic.c

--- clinic.c
+++ clinic.c
@@ -1,16 +1,3 @@
-/*/////////////////////////////////////////////////////////////////////////
-                        Assignment 1 - Milestone 3
-Full Name  : Christine Ang
-Student ID#: 121559223
-Email      : cang4@myseneca.ca
-Section    : NAA
-
-Authenticity Declaration:
-I declare this submission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. This submitted
-piece of work is entirely of my own creation.
-/////////////////////////////////////////////////////////////////////////*/
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
